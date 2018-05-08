6760d8525aa06d2c509e2fe84a386707c8eb69bd
Ethan Lee <ethanlee515@gmail.com>
2018-05-07 23:51:30 -0400

    tried to make anime

--- animelist.py
+++ animelist.py
@@ -1,13 +1,53 @@
 import re
 import requests
+import json
+import pprint
 
 def getTitle(id):
     s = requests.get("https://myanimelist.net/anime/" + str(id)).text
     p_title = r'<span itemprop="name">(.*)</span'
     return re.findall(p_title, s)[0]
 
+def getJson(html):
+    p_json = r'data-items="([^"]*)">'
+    return json.loads(re.findall(p_json, html)[0].replace(r'\/', '/').replace(r'&quot;', '"'))
 
+def makeVec(json):
+    scores = dict()
+    for anime in json:
+        score = anime['score']
+        if score != 0:
+            scores['anime_id'] = score
+    # TODO normalize and make z-scores
+    return scores
 
+#TODO rewrite me
+def userToVec(username):
+    try:
+        s = requests.get("https://myanimelist.net/animelist/" + username + "?status=7").text
+    except Exception:
+        print("Dead link: " + username)
+        return dict()
+    rows = list()
+    index = 0
+    while True:
+        index = s.find("<tr", index)
+        if index == -1:
+            break
+        startRow = s.find(">", index) + 1
+        endRow = s.find("</tr", startRow)
+        rows.append(s[startRow: endRow])
+        index = endRow
+
+    print(s)
+
+    #TODO this is placeholder
+    # return {"PH_1": randint(1, 10), "PH_2": randint(1, 10)}
+if __name__ == "__main__":
+    with open("out.html") as f:
+        s = f.read()
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(getJson(s))
 
 # print(getTitle(28819))
 # print(getTitle(36456))
--- animerec.py
+++ animerec.py
@@ -11,11 +11,11 @@ if len(sys.argv) != 2:
     exit(0)
 
 username = sys.argv[1]
-
-#TODO rewrite me
-def userToVec(username):
-    #TODO this is placeholder
-    return {"PH_1": randint(1, 10), "PH_2": randint(1, 10)}
+try:
+    uservec = animelist.userToVec(username)
+except Exception:
+    print("cannot find " + username + " on MyAnimeList")
+    exit(0)
 
 def dot(v1, v2):
     s = 0
@@ -24,8 +24,27 @@ def dot(v1, v2):
             s += v1[key] * v2[key]
     return s
 
-def sim(v1, v2):
+def cos_sim(v1, v2):
     d = dot(v1, v2)
     if d == 0:
         return 0
     return d / sqrt(dot(v1, v1) * dot(v2, v2))
+
+sim = dot #TODO command line flag
+
+users = dict() #TODO populate this
+
+r = rankings.rankings(50)
+
+for user in users:
+    if user.name == username:
+        continue
+    r.insert(user.vec, sim(user.vec, uservec))
+
+# now use r...
+lst = r.getList()
+for vec in lst:
+    
+    pass
+
+"myanimelist.net/anime/36456"
