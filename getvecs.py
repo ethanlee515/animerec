5b1acf4e4a08bcce7244ca59775dbf51e4939adb
Yi Lee <ylee157@ugrad11.cs.jhu.edu>
2018-05-11 17:09:43 -0400

    way to turn usernames into vecs

---
+++ getvecs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import animelist
+
+if len(sys.argv) != 3:
+    print("usage: getvecs.py [usersfile] [vecsfile]")
+    exit(0)
+
+with open(sys.argv[1]) as f:
+    users = []
+    for line in f:
+        users.append(line.strip())
+    
+with open(sys.argv[1], "w") as f:
+    for user in users[5:]:
+        f.write(user + '\n')
+
+users = users[:5]
+
+with open(sys.argv[2]) as f:
+    d = json.load(f)
+
+with open(sys.argv[2], "w+") as f:
+    for user in users:
+        if user not in d:
+            d[user] = animelist.userToVec(user)
+    json.dump(d, f)
+
